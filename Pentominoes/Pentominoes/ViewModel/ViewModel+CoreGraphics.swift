21c22d24e6f01a58f44587eba4adbbdae163a4c3
Michael Tufillaro <mjt6260@psu.edu>
2026-02-28 20:06:38 -0500

    more work on pieceView. Figured out how to adjust position to center like in hint 17

--- Pentominoes/Pentominoes/ViewModel/ViewModel+CoreGraphics.swift
+++ Pentominoes/Pentominoes/ViewModel/ViewModel+CoreGraphics.swift
@@ -17,5 +17,34 @@ extension ViewModel
         return CGFloat(coord) * self.blockSize
     }
     
-    //TODO: function that takes in a Position and returns a CGPoint
+    
+    //function that takes in a Piece and returns a CGPoint representing its location, centered to the middle of the piece
+    func pieceToCGPoint(piece: Piece) -> CGPoint
+    {
+        //calculate the top left of the rectangle that contains the piece
+        let topLeftX = self.unitToViewCoord(coord: piece.position.x)
+        let topRightY = self.unitToViewCoord(coord: piece.position.y)
+        
+        //calculate the height and width of the Piece
+        var pieceHeight: CGFloat
+        var pieceWidth: CGFloat
+        
+        switch piece.position.orientation
+        {
+            //if the piece is on its side, the height and width are swapped
+            case .left, .right, .leftMirrored, .rightMirrored:
+                pieceWidth = unitToViewCoord(coord: piece.outline.size.height)
+                pieceHeight = unitToViewCoord(coord: piece.outline.size.width)
+            //otherwise, the height and width are normal
+            default:
+                pieceHeight = unitToViewCoord(coord: piece.outline.size.height)
+                pieceWidth = unitToViewCoord(coord: piece.outline.size.width)
+            
+        }
+        //calculate the center of the piece view
+        let centerX = topLeftX + (pieceWidth / 2)
+        let centerY = topRightY + (pieceHeight / 2)
+        
+        return CGPoint(x: centerX, y: centerY)
+    }
 }
--- Pentominoes/Pentominoes/Views/PieceView.swift
+++ Pentominoes/Pentominoes/Views/PieceView.swift
@@ -20,11 +20,13 @@ struct PieceView: View
         //calculate the proper width and height of the piece based on the block size and the size of the piece
         let pieceWidth = manager.unitToViewCoord(coord: piece.outline.size.width)
         let pieceHeight = manager.unitToViewCoord(coord: piece.outline.size.height)
+        //call helper func to get the position of the piece in terms of a CGPoint (non-unit values)
+        let translatedPosition = manager.pieceToCGPoint(piece: piece)
         //make a pentominoView based on the Piece's outline, and sized correctly
         PentominoView(pentominoOutline: piece.outline)
             .frame(width: pieceWidth, height: pieceHeight)
-            //TODO: turn the Piece's position into a CGPoint in VM
-            //.position()
+            .position(translatedPosition)
+        //TODO: figure out how to rotate pieces if the orientation is different
     }
 }
 
