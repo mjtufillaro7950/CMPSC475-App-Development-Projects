ef0220cd467953511add720b4a9a181926895742
Michael Tufillaro <mjt6260@psu.edu>
2026-03-29 18:17:02 -0400

    Network Manager done

--- Pokedex/Pokedex/ViewModels/NetworkManager.swift
+++ Pokedex/Pokedex/ViewModels/NetworkManager.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 
 @Observable
-class NetworkManager {
+class NetworkManager
+{
     static let ipAddress : String = "http://localhost:8000"
     private var authManager: AuthManager?
     
+    //TODO: not sure if this is right, but idk how else to get the email and password to the login func
     //temporarily store the user's email and password while logging in for the login func
     var loginEmail: String = ""
     var loginPassword: String = ""
@@ -26,14 +28,13 @@ class NetworkManager {
     //Implement API request for user login- largely taken from class code with some modifications
     func login() async throws
     {
-        //fetches the right url for the Swagger website using the network managers ip address
+        //fetches the right url for the Swagger website using the network managers ip address (need to do NetworkManager.ipAddress because it's not static)
         guard let url = URL(string: "\(NetworkManager.ipAddress)/auth/login") else
         {
             throw NetworkError.invalidURL
         }
         
         //use passed in data to create the payload
-        //TODO: login didn't have parameters, meaning that the email and password need to be stored in here?
         let payload = LoginRequest(email: self.loginEmail, password: self.loginPassword)
         
         //build a request for a login
@@ -68,26 +69,146 @@ class NetworkManager {
         //attempt to deode the data from the response into a TokenResponse
         let decoder = JSONDecoder()
         let tokenResponse = try decoder.decode(TokenResponse.self, from: data)
-        //instead of returning the tokenResponse, call setUser in AuthManager
+        //instead of returning the tokenResponse, set User in authmanager
         authManager?.setUser(email: tokenResponse.user.email, token: tokenResponse.accessToken)
     }
     
-    func getPokemonCollection() async throws -> [Pokemon] {
-        //TODO: Implement API request to get Pokemon collection
+    
+    //adapted frrom Taskly getTasks()
+    func getPokemonCollection() async throws -> [Pokemon]
+    {
+        //Implement API request to get Pokemon collection
+        //mostly the same as login
+        guard let url = URL(string: "\(NetworkManager.ipAddress)/pokemon") else
+        {
+            throw NetworkError.invalidURL
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "accept")
+        
+        //if there is a token, attatch it to the request
+        if let token = authManager?.accessToken
+        {
+            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        }
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse else
+        {
+            throw NetworkError.invalidResponse
+        }
+        
+        if httpResponse.statusCode == 401
+        {
+            throw NetworkError.unauthorized
+        }
+        
+        guard (200...299).contains(httpResponse.statusCode) else
+        {
+            throw NetworkError.httpError(statusCode: httpResponse.statusCode)
+        }
+        
+        let decoder = JSONDecoder()
+        //decode the data into an array of Pokemon objects and return it
+        return try decoder.decode([Pokemon].self, from: data)
     }
 
-    func getPokemon(id: Int) async throws -> Pokemon {
-        //TODO: Implement API request to get a specific Pokemon by their id
+    
+    func getPokemon(id: Int) async throws -> Pokemon
+    {
+        //Implement API request to get a specific Pokemon by their id
+        //pretty much the same as the above function except it goes to a different url and returns a single Pokemon object
+        guard let url = URL(string: "\(NetworkManager.ipAddress)/pokemon/\(id)") else
+        {
+            throw NetworkError.invalidURL
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "accept")
+        
+        //if there is a token, attatch it to the request
+        if let token = authManager?.accessToken
+        {
+            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        }
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse else
+        {
+            throw NetworkError.invalidResponse
+        }
+        
+        if httpResponse.statusCode == 401
+        {
+            throw NetworkError.unauthorized
+        }
+        
+        guard (200...299).contains(httpResponse.statusCode) else
+        {
+            throw NetworkError.httpError(statusCode: httpResponse.statusCode)
+        }
+        
+        let decoder = JSONDecoder()
+        //decode the data into a Pokemon object and return it
+        return try decoder.decode(Pokemon.self, from: data)
     }
     
-    static func getImageURL(for pokemonId: Int) -> URL? {
+    
+    static func getImageURL(for pokemonId: Int) -> URL?
+    {
         // use this URL in AsyncImage
-        return URL(string: "\(ipAddress)/pokemons/\(pokemonId)/image")
+        //this said "pokemons" but I'm pretty sure it needs to be "pokemon" singular
+        return URL(string: "\(ipAddress)/pokemon/\(pokemonId)/image")
     }
     
-    func setCapture(for pokemonId: Int, isCaptured: Bool) async throws {
-        //TODO: implement capture/release of Pokemon using API endpoints pokemon/{id}/capture and pokemon/{id}/release
+    
+    func setCapture(for pokemonId: Int, isCaptured: Bool) async throws
+    {
+        //implement capture/release of Pokemon using API endpoints pokemon/{id}/capture and pokemon/{id}/release
+        //depending on isCaptured, change the endpoint of the URL
+        var capturedEndpoint: String
+        if isCaptured {capturedEndpoint = "capture"}
+        else {capturedEndpoint = "release"}
+        
+        //set up the URL to the right endpoint
+        guard let url = URL(string: "\(NetworkManager.ipAddress)/pokemon/\(pokemonId)/\(capturedEndpoint)") else
+        {
+            throw NetworkError.invalidURL
+        }
+        var request = URLRequest(url: url)
+        
+        //need to use PUT for capture/release
+        request.httpMethod = "PUT"
+        request.setValue("application/json", forHTTPHeaderField: "accept")
+        
+        //if there is a token, attatch it to the request
+        if let token = authManager?.accessToken
+        {
+            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        }
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse else
+        {
+            throw NetworkError.invalidResponse
+        }
+        
+        if httpResponse.statusCode == 401
+        {
+            throw NetworkError.unauthorized
+        }
+        
+        guard (200...299).contains(httpResponse.statusCode) else
+        {
+            throw NetworkError.httpError(statusCode: httpResponse.statusCode)
+        }
     }
+    
     
     //NetworkError Enum taken from class code
     enum NetworkError: LocalizedError
@@ -118,7 +239,6 @@ class NetworkManager {
             }
         }
     }
-    
 }
 
 
